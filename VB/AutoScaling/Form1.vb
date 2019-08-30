cedba4fda1e19a98e7d44203dc7e5ca42669df78
DevExpressExampleBot <fakeEmail>
2019-08-30 14:17:33 +0300

    Source auto update [skip ci]

---
+++ VB/AutoScaling/Form1.vb
@@ -0,0 +1,30 @@
+﻿Imports System
+Imports System.Windows.Forms
+Imports DevExpress.XtraPrinting
+Imports DevExpress.XtraReports.UI
+Imports DevExpress.XtraPrinting.Preview
+' ...
+
+Namespace AutoScaling
+	Partial Public Class Form1
+		Inherits Form
+
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			Dim printTool As New ReportPrintTool(New XtraReport1())
+			printTool.Report.CreateDocument(False)
+			AddHandler printTool.PreviewForm.Load, AddressOf PreviewForm_Load
+			printTool.ShowPreviewDialog()
+		End Sub
+
+		Private Sub PreviewForm_Load(ByVal sender As Object, ByVal e As EventArgs)
+			Dim frm As PrintPreviewFormEx = DirectCast(sender, PrintPreviewFormEx)
+			frm.PrintingSystem.ExecCommand(PrintingSystemCommand.Scale, New Object() { 0.7F })
+		End Sub
+
+	End Class
+End Namespace
+
---
+++ VB/AutoScaling/XtraReport1.Designer.vb
@@ -0,0 +1,110 @@
+﻿Namespace AutoScaling
+	Partial Public Class XtraReport1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary> 
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.components = New System.ComponentModel.Container()
+			Dim selectQuery1 As New DevExpress.DataAccess.Sql.SelectQuery()
+			Dim column1 As New DevExpress.DataAccess.Sql.Column()
+			Dim columnExpression1 As New DevExpress.DataAccess.Sql.ColumnExpression()
+			Dim table1 As New DevExpress.DataAccess.Sql.Table()
+			Dim column2 As New DevExpress.DataAccess.Sql.Column()
+			Dim columnExpression2 As New DevExpress.DataAccess.Sql.ColumnExpression()
+			Dim resources As New System.ComponentModel.ComponentResourceManager(GetType(XtraReport1))
+			Me.Detail = New DevExpress.XtraReports.UI.DetailBand()
+			Me.xrLabel1 = New DevExpress.XtraReports.UI.XRLabel()
+			Me.PageHeader = New DevExpress.XtraReports.UI.PageHeaderBand()
+			Me.PageFooter = New DevExpress.XtraReports.UI.PageFooterBand()
+			Me.sqlDataSource1 = New DevExpress.DataAccess.Sql.SqlDataSource(Me.components)
+			CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+			' 
+			' Detail
+			' 
+			Me.Detail.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() { Me.xrLabel1})
+			Me.Detail.HeightF = 48F
+			Me.Detail.Name = "Detail"
+			Me.Detail.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.Detail.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' xrLabel1
+			' 
+			Me.xrLabel1.ExpressionBindings.AddRange(New DevExpress.XtraReports.UI.ExpressionBinding() { New DevExpress.XtraReports.UI.ExpressionBinding("BeforePrint", "Text", "[ProductName]")})
+			Me.xrLabel1.LocationFloat = New DevExpress.Utils.PointFloat(25F, 8F)
+			Me.xrLabel1.Name = "xrLabel1"
+			Me.xrLabel1.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+			Me.xrLabel1.SizeF = New System.Drawing.SizeF(342F, 25F)
+			Me.xrLabel1.Text = "xrLabel1"
+			' 
+			' PageHeader
+			' 
+			Me.PageHeader.HeightF = 30F
+			Me.PageHeader.Name = "PageHeader"
+			Me.PageHeader.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.PageHeader.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' PageFooter
+			' 
+			Me.PageFooter.HeightF = 30F
+			Me.PageFooter.Name = "PageFooter"
+			Me.PageFooter.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.PageFooter.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' sqlDataSource1
+			' 
+			Me.sqlDataSource1.ConnectionName = "AutoScaling.Properties.Settings.nwindConnectionString"
+			Me.sqlDataSource1.Name = "sqlDataSource1"
+			columnExpression1.ColumnName = "ProductID"
+			table1.Name = "Products"
+			columnExpression1.Table = table1
+			column1.Expression = columnExpression1
+			columnExpression2.ColumnName = "ProductName"
+			columnExpression2.Table = table1
+			column2.Expression = columnExpression2
+			selectQuery1.Columns.Add(column1)
+			selectQuery1.Columns.Add(column2)
+			selectQuery1.Name = "Products"
+			selectQuery1.Tables.Add(table1)
+			Me.sqlDataSource1.Queries.AddRange(New DevExpress.DataAccess.Sql.SqlQuery() { selectQuery1})
+			Me.sqlDataSource1.ResultSchemaSerializable = resources.GetString("sqlDataSource1.ResultSchemaSerializable")
+			' 
+			' XtraReport1
+			' 
+			Me.Bands.AddRange(New DevExpress.XtraReports.UI.Band() { Me.Detail, Me.PageHeader, Me.PageFooter})
+			Me.ComponentStorage.AddRange(New System.ComponentModel.IComponent() { Me.sqlDataSource1})
+			Me.DataMember = "Products"
+			Me.DataSource = Me.sqlDataSource1
+			Me.DrawGrid = False
+			Me.Version = "17.2"
+			CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+
+		End Sub
+
+		#End Region
+
+		Private Detail As DevExpress.XtraReports.UI.DetailBand
+		Private PageHeader As DevExpress.XtraReports.UI.PageHeaderBand
+		Private PageFooter As DevExpress.XtraReports.UI.PageFooterBand
+		Private xrLabel1 As DevExpress.XtraReports.UI.XRLabel
+		Private sqlDataSource1 As DevExpress.DataAccess.Sql.SqlDataSource
+	End Class
+End Namespace
